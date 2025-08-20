796d43af8954e5b5a739a7b6ef134bfbd455ba7e
iwaeiw <marvelous.rust@yandex.ru>
2025-08-21 00:01:31 +0500

    added cart, context_processors done

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -23,8 +24,14 @@ def product_detail(request, id, slug):
     related_products = Product.objects.filter(
         category=product.category, available=True
     ).exclude(id=product.id)[:4]
-    return render[
+
+    cart_product_form = CartAddProductForm()
+    return render(
         request,
-        "main/prodcut/detail.html",
-        {"product": product, "related_products": related_products},
-    ]
+        "main/product/detail.html",
+        {
+            "product": product,
+            "related_products": related_products,
+            "cart_product_form": cart_product_form,
+        },
+    )
